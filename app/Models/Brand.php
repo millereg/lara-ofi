4d84112ed812d49f768fc75370da52c44e5015ee
millereg <gemillert4090@outlook.com>
2025-02-17 21:10:21 -0500

    Configuration Models and Migrations

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'is_active'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
